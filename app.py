a9ad8bd83885add5b32d6fee70d9dd95c917c562
deepakawsm24-bit <deepakawsm24@gmail.com>
2026-08-08 22:34:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,31 +3,66 @@ import tensorflow as tf
 import numpy as np
 from PIL import Image
 
-# Page Title
-st.set_page_config(page_title="Brain Tumor Detection", page_icon="🧠")
-st.title("🧠 Brain Tumor Detection")
+# Page configuration
+st.set_page_config(
+    page_title="Brain Tumor Detection",
+    page_icon="🧠"
+)
 
-# Load Trained Model
-model = tf.keras.models.load_model("brain_tumor_model.keras")
+# Title
+st.title("🧠 Brain Tumor Detection")
 st.write("Upload an MRI Brain Image to Detect Tumor")
 
+# Load trained model
+model = tf.keras.models.load_model("brain_tumor_model.keras")
+
 # Upload MRI Image
-uploaded_file = st.file_uploader("Upload Brain MRI Image",type = ["jpg","jpeg","png"])
+uploaded_file = st.file_uploader(
+    "Upload Brain MRI Image",
+    type=["jpg", "jpeg", "png"]
+)
 
 # Class Names
-class_names = ["glioma_tumor", "meningioma_tumor",  "no_tumor", "pituitary_tumor"]
+class_names = [
+    "glioma_tumor",
+    "meningioma_tumor",
+    "no_tumor",
+    "pituitary_tumor"
+]
 
 if uploaded_file is not None:
-  image = Image.open(uploaded_file)
-  st.image(image,caption = "Uploaded MRI Image", use_column_width=True)
-  img = image.resize((224,224))
-  img = np.array(img)/255.0
-  img = np.expand_dims(img,axis=0)
-  prediction = model.predict(img)
-  predicted_class = class_names[np.argmax(prediction)]
-  confidence = np.max(prediction)*100
-  st.success(f"Prediction:{predicted_class}")
-  st. write(f"Confidence:{condfidence:.2f}%")
+
+    # Open image
+    image = Image.open(uploaded_file).convert("RGB")
+
+    # Display uploaded image
+    st.image(
+        image,
+        caption="Uploaded MRI Image",
+        width="stretch"
+    )
+
+    # Resize image
+    img = image.resize((224, 224))
+
+    # Convert image to numpy array
+    img = np.array(img) / 255.0
+
+    # Add batch dimension
+    img = np.expand_dims(img, axis=0)
+
+    # Prediction
+    prediction = model.predict(img)
+
+    # Get predicted class
+    predicted_class = class_names[np.argmax(prediction)]
+
+    # Get confidence
+    confidence = float(np.max(prediction)) * 100
+
+    # Display result
+    st.success(f"Prediction: {predicted_class}")
+    st.write(f"Confidence: {confidence:.2f}%")
 
 
 
